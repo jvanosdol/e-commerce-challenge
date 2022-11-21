96395feab1773959589b392a45c9d8ad680a3fc5
jvanosdol <j.vanosdol99@yahoo.com>
2022-11-21 12:57:57 -0800

    finished product-routes.js file

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -110,6 +110,23 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id,
+    }
+  })
+  .then(data => {
+    if(!data) {
+      res.status(404).json({ message: 'There is no product with this id'})
+      return
+    }
+    res.json(data)
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).json(err)
+  })
+
 });
 
 module.exports = router;
